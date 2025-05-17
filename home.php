82be8c36df85bfe0ef9b69a83ba48964ba3233ea
Mitsumandot <chiguer.othmane@ensam-casa.ma>
2025-05-18 00:28:16 +0100

    home page creation and adding actions selection

--- home.php
+++ home.php
@@ -1,7 +1,31 @@
 <?php 
+if(isset($_POST["action"])){
+    header("Location: actions/" . $_POST["action"] . ".php");
+    exit();
+}
 session_start();
 echo "Bonjour ".$_SESSION["nom"]." ".$_SESSION["prenom"];
 
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post">
+        Choisir une acion parmi les actions suivantes :
+        <select name="action">
+            <option value="addCar">Ajouter voiture</option>
+            <option value="addRental">Ajouter location</option>
+            <option value="addClient">Ajouter client</option>
+        </select>
+        <input type="submit" value="choisir">
+    </form>
+</body>
+</html>
